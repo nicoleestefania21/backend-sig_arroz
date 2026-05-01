d892b5e85112b31d79eb31116f0e7d063f212ffe
andresnico44 <ramiroandresnc@ufps.edu.co>
2026-05-01 11:27:20 -0500

    Ajustes en los envíos de los correos electrónicos

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     "rest_framework_simplejwt",
+    "anymail",
     "users",
     "core",
 ]
@@ -129,15 +130,13 @@ if DEBUG:
         "SIGARROZ <noreply@local.test>",
     )
 else:
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
-    EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
-    EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
-    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+    EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
+    ANYMAIL = {
+        "RESEND_API_KEY": os.environ.get("RESEND_API_KEY"),
+    }
     DEFAULT_FROM_EMAIL = os.environ.get(
         "DEFAULT_FROM_EMAIL",
-        f"SIGARROZ <{EMAIL_HOST_USER}>"
+        "SIGARROZ <onboarding@resend.dev>",
     )
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
